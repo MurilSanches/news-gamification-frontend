5366ca67a96fc3aa77c99c35cff8059efe107969
Murilo Sanches <murilo.de.paula2011@gmail.com>
2025-02-14 20:04:36 -0300

    base project

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: ['./src/**/*.{mjs,js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      yellow: '#FFCE04',
+      black: '#000000',
+      brown: '#240E0B',
+      gray: '#615A5A',
+      white: '#FFFFFF'
+    },
+    extend: {}
+  },
+  plugins: []
+}
